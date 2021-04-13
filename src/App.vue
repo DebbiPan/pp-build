e02ec40e06b376a7cd20768fea9569382356c86a
Debbie <1770554944@qq.com>
2021-04-13 18:22:56 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>hi</div>
+  <Debbie />
+</template>
+
+<script>
+import Debbie from './components/Debbie.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Debbie
+  }
+}
+</script>
