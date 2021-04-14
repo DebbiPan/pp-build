4724befa5e7368781df0b3cc4d2ca9e0807bc38c
Debbie <1770554944@qq.com>
2021-04-14 15:34:02 +0800

    更改了topnav样式，并且完成home样式

---
+++ src/components/Topnav.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="topnav">
+    <div class="logo">
+      <img src="../assets/img/logopic.png" alt="">
+    </div>
+    <ul class="menu">
+      <li>菜单1</li>
+      <li>菜单2</li>
+    </ul>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.topnav{
+  background:#ffffff;
+  display:flex;
+  padding:16px;
+  border-bottom:1px solid #DCDFE6;
+  > .logo{
+    max-width:32px;
+    margin-right:auto;
+    > img{
+      max-width:32px;
+      max-height:32px;
+    }
+  }
+  > .menu{
+    display: flex;
+    white-space: nowrap;
+    flex-wrap: nowrap;
+    color:#8CC4FD;
+    > li{
+      margin:0 1em;
+      line-height:32px;
+    }
+  }
+}
+</style>
