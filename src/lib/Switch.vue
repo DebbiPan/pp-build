4ee9710d6a3a6fbbbb6a022698b68b514ace2439
Debbie <1770554944@qq.com>
2021-04-16 10:23:27 +0800

    hover之后切换

---
+++ src/lib/Switch.vue
@@ -0,0 +1,32 @@
+<template>
+  <button><span></span></button>
+</template>
+
+<script lang="ts">
+
+</script>
+
+<style lang="scss" scoped>
+$h:22px;
+$h2:$h - 4px;
+button{
+  height:$h;
+  width:$h*2;
+  border:none;
+  background:#1890FF;
+  border-radius:$h/2;
+  position: relative;
+}
+span{
+  position: absolute;
+  height:$h2;
+  width:$h2;
+  background: #fff;
+  top:2px;
+  left:2px;
+  border-radius: $h2/2;
+}
+button:hover > span{
+  left:calc(100% - #{$h2} - 2px)
+}
+</style>
