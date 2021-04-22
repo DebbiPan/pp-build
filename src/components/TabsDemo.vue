76cc7d00f6cd5f6c5bfec6310438de6c498b9e4c
Debbie <1770554944@qq.com>
2021-04-22 12:06:07 +0800

    检查Tabs的子组件的类型是Tab

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -2,8 +2,8 @@
   <div>Tabs示例</div>
   <h1>示例1</h1>
   <Tabs>
-    <tab title="导航1">内容1</tab>
-    <tab title="导航2">内容2</tab>
+    <Tab title="导航1">内容1</Tab>
+    <Tab title="导航2">内容2</Tab>
   </Tabs>
 </template>
 
--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -2,3 +2,19 @@
   <div>Tabs组件</div>
 </template>
 
+<script lang="ts">
+import Tab from "./Tab.vue"
+export default {
+  setup(props,context){
+    const defaults = context.slots.default()
+    defaults.forEach((tag)=>{
+      if(tag.type !== Tab){
+        throw new Error('Tabs子标签必须是Tab')
+      }
+    })
+    return {
+      defaults
+    }
+  }
+}
+</script>
