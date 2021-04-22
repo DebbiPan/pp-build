f45cf2e3a7281f508d7187f4849170e93f32464d
Debbie <1770554944@qq.com>
2021-04-22 15:14:30 +0800

    实现tab切换

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,20 +1,81 @@
 <template>
-  <div>Tabs组件</div>
+  <div class="build-tabs">
+    <div class="build-tabs-nav">
+      <div class="build-tabs-nav-item " v-for="(t,index) in titles" :key="index"
+           :class="{selected: t === selected}" @click="select(t)">{{ t }}
+      </div>
+    </div>
+    <div class=" build-tabs-content">
+      <component :is="current" :key="current.props.title"></component>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import Tab from "./Tab.vue"
+import Tab from './Tab.vue';
+import {computed} from 'vue';
+
 export default {
-  setup(props,context){
-    const defaults = context.slots.default()
-    defaults.forEach((tag)=>{
-      if(tag.type !== Tab){
-        throw new Error('Tabs子标签必须是Tab')
+  props: {
+    selected: {
+      type: String
+    }
+  },
+  setup(props, context) {
+    const defaults = context.slots.default();
+    defaults.forEach((tag) => {
+      if (tag.type !== Tab) {
+        throw new Error('Tabs子标签必须是Tab');
       }
-    })
+    });
+    const titles = defaults.map((tag) => {
+      return tag.props.title;
+    });
+    const current = computed(()=>{
+      return defaults.filter((tag)=>{
+        return tag.props.title === props.selected
+      })[0]
+    });
+    const select = (title:string)=>{
+      context.emit('update:selected',title)
+    }
     return {
-      defaults
+      defaults,
+      titles,
+      current,
+      select
+    };
+  }
+};
+</script>
+
+<style lang="scss">
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+.build-tabs {
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
+
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+
+      &:first-child {
+        margin-left: 0;
+      }
+
+      &.selected {
+        color: $blue;
+      }
     }
   }
+
+  &-content {
+    padding: 8px 0;
+  }
 }
-</script>
+</style>
