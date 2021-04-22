8d08486f0509d6e2630c95160af28310ab426322
Debbie <1770554944@qq.com>
2021-04-22 16:38:38 +0800

    完成下划线移动

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="build-tabs">
-    <div class="build-tabs-nav">
+    <div class="build-tabs-nav" ref="container">
       <div class="build-tabs-nav-item " v-for="(t,index) in titles" :key="index"
+           :ref="el => {if (el) navItems[index] = el}"
            :class="{selected: t === selected}" @click="select(t)">{{ t }}
       </div>
+      <div class="build-tabs-nav-indicator" ref="indicator"></div>
     </div>
     <div class=" build-tabs-content">
       <component :is="current" :key="current.props.title"></component>
@@ -13,7 +15,7 @@
 
 <script lang="ts">
 import Tab from './Tab.vue';
-import {computed} from 'vue';
+import {computed, ref, onMounted, onUpdated} from 'vue';
 
 export default {
   props: {
@@ -22,6 +24,22 @@ export default {
     }
   },
   setup(props, context) {
+    const navItems = ref<HTMLDivElement[]>([]);
+    const indicator = ref<HTMLDivElement>(null);
+    const container = ref<HTMLDivElement>(null);
+    const x = () => {
+      const divs = navItems.value;
+      const result = divs.filter(div => div.classList.contains('selected'))[0];
+      const {width} = result.getBoundingClientRect();
+      indicator.value.style.width = width + 'px';
+      const {left: left1} = container.value.getBoundingClientRect();
+      const {left: left2} = result.getBoundingClientRect();
+      const left = left2 - left1;
+      indicator.value.style.left = left + 'px';
+    };
+    onMounted(x);
+    onUpdated(x);
+
     const defaults = context.slots.default();
     defaults.forEach((tag) => {
       if (tag.type !== Tab) {
@@ -31,19 +49,22 @@ export default {
     const titles = defaults.map((tag) => {
       return tag.props.title;
     });
-    const current = computed(()=>{
-      return defaults.filter((tag)=>{
-        return tag.props.title === props.selected
-      })[0]
+    const current = computed(() => {
+      return defaults.filter((tag) => {
+        return tag.props.title === props.selected;
+      })[0];
     });
-    const select = (title:string)=>{
-      context.emit('update:selected',title)
-    }
+    const select = (title: string) => {
+      context.emit('update:selected', title);
+    };
     return {
       defaults,
       titles,
       current,
-      select
+      select,
+      navItems,
+      indicator,
+      container
     };
   }
 };
@@ -58,6 +79,7 @@ $border-color: #d9d9d9;
     display: flex;
     color: $color;
     border-bottom: 1px solid $border-color;
+    position: relative;
 
     &-item {
       padding: 8px 0;
@@ -71,6 +93,16 @@ $border-color: #d9d9d9;
       &.selected {
         color: $blue;
       }
+    }
+
+    &-indicator {
+      position: absolute;
+      height: 3px;
+      background: $blue;
+      left: 0;
+      bottom: -1px;
+      width: 50px;
+      transition: all 250ms;
     }
   }
 
