7b699b3a58064a5b885da4894d5412a7c30d0e0e
Debbie <1770554944@qq.com>
2021-04-26 18:34:13 +0800

    改造DialogDemo

---
+++ src/components/Dialog2.demo.vue
@@ -0,0 +1,37 @@
+<demo>一键打开Dialog</demo>
+
+<template>
+  <div>
+    <Button @click="showDialog">打开对话框</Button>
+  </div>
+</template>
+
+<script lang="ts">
+import Button from '../lib/Button.vue';
+import {openDialog} from '../lib/openDialog';
+import {h} from 'vue';
+
+export default {
+  components: {
+    Button
+  },
+  setup() {
+    const showDialog = () => {
+      openDialog({
+        title: h('strong', {}, '标题'),
+        content: '你好',
+        ok() {
+          console.log('ok');
+        },
+        cancel() {
+          console.log('cancel');
+        }
+      });
+    };
+
+    return {
+      showDialog
+    };
+  }
+};
+</script>
