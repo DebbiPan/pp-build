f5662b5eae29dc684012fbd853db9affc514a8fa
Debbie <1770554944@qq.com>
2021-04-26 18:00:56 +0800

    改造Button

---
+++ src/components/Button5.demo.vue
@@ -0,0 +1,21 @@
+<demo>支持加载中</demo>
+<template>
+  <div>
+    <Button loading>加载中</Button>
+    <Button>加载完毕</Button>
+  </div>
+</template>
+
+<script lang="ts">
+import Button from "../lib/Button.vue"
+
+export default {
+  components:{
+    Button
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
